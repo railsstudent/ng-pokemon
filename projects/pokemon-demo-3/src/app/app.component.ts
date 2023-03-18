5a127808468cba4468676c11f2afb2ecf02badab
Connie Leung <cleung334@gmail.com>
2023-03-18 23:06:42 +0800

    feat: pokemon demo 3

---
+++ projects/pokemon-demo-3/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { PokemonComponent } from './pokemon/pokemon/pokemon.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [PokemonComponent],
+  template: '<app-pokemon></app-pokemon>',
+  styles: [`
+    :host {
+      display: block;
+    }
+  `]
+})
+export class AppComponent {
+  constructor(titleService: Title) {
+    const title = 'Pokemon Demo 3';
+
+    titleService.setTitle(title);
+  }
+}
