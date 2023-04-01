242362e0924fb5261bd20f377d7b8348e05e1f82
Connie Leung <cleung334@gmail.com>
2023-04-02 00:47:24 +0800

    feat: ngComponentOutlet

--- projects/pokemon-demo-9/src/app/pokemon/pokemon-tab/pokemon-tab.component.ts
+++ projects/pokemon-demo-9/src/app/pokemon/pokemon-tab/pokemon-tab.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Injector, Input, QueryList, ViewChildren, inject } from '@angular/core';
+import { fromEvent, map, merge } from 'rxjs';
+import { POKEMON_OBJ } from '../constants/pokemon.constant';
 import { FlattenPokemon } from '../interfaces/pokemon.interface';
 import { PokemonAbilitiesComponent } from '../pokemon-abilities/pokemon-abilities.component';
 import { PokemonStatsComponent } from '../pokemon-stats/pokemon-stats.component';
@@ -6,13 +9,13 @@ import { PokemonStatsComponent } from '../pokemon-stats/pokemon-stats.component'
 @Component({
   selector: 'app-pokemon-tab',
   standalone: true,
-  imports: [PokemonAbilitiesComponent, PokemonStatsComponent],
+  imports: [PokemonAbilitiesComponent, PokemonStatsComponent, NgComponentOutlet],
   template: `
     <div style="padding: 0.5rem;">
       <ul>
-        <li><a href="#" data-type="all">All</a></li>
-        <li><a href="#" data-type="statistics">Stats</a></li>
-        <li><a href="#" data-type="abilities">Abilities</a></li>
+        <li><a href="#" #selection data-type="all">All</a></li>
+        <li><a href="#"  #selection data-type="statistics">Stats</a></li>
+        <li><a href="#" #selection data-type="abilities">Abilities</a></li>
       </ul>
     </div>
     <app-pokemon-abilities [pokemon]="pokemon"></app-pokemon-abilities>
@@ -34,7 +37,43 @@ import { PokemonStatsComponent } from '../pokemon-stats/pokemon-stats.component'
     }
   `]
 })
-export class PokemonTabComponent {
+export class PokemonTabComponent implements AfterViewInit {
   @Input()
   pokemon!: FlattenPokemon;
+
+  @ViewChildren('selection', { read: ElementRef })
+  selections!: QueryList<ElementRef<HTMLLinkElement>>;
+
+  PokemonAbilitiesComponent = PokemonAbilitiesComponent;
+  PokemonStatsComponent = PokemonStatsComponent;
+
+  injector = inject(Injector);
+  myInjector = Injector.create({
+    providers: [{
+      provide: POKEMON_OBJ,
+      useValue: this.pokemon,
+    }],
+    parent: this.injector
+  });
+
+  ngAfterViewInit(): void {
+    const linkClicked$ = this.selections.map(({ nativeElement }) => {
+      const type = nativeElement.dataset['type'];
+      return fromEvent(nativeElement, 'click').pipe(map(() => type))
+    });
+
+    merge(...linkClicked$)
+      .pipe(
+        map((selection) => {
+          const myInjector = Injector.create({
+            providers: [{
+              provide: POKEMON_OBJ,
+              useValue: this.pokemon,
+            }],
+            parent: this.injector
+          });          
+        })
+      )
+      .subscribe((value) => console.log(value));
+  }
 }
