189795cc4a0663001defa020b0732175589617ac
Connie Leung <cleung334@gmail.com>
2023-05-05 23:58:00 +0800

    feat: create new standalone components

--- projects/pokemon-demo-13/src/app/app.route.ts
+++ projects/pokemon-demo-13/src/app/app.route.ts
@@ -4,5 +4,14 @@ export const APP_ROUTES: Route[] = [
     {
       path: 'pokemon',
       loadComponent: () => import('./pokemon/pokemon/pokemon.component').then(c => c.PokemonComponent)
+    },
+    {
+      path: '',
+      pathMatch: 'full',
+      loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
+    },
+    {
+      path: '**',
+      loadComponent: () => import('./page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)
     }
 ];
