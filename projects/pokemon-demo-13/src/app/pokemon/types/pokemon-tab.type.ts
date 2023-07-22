817fc4724202f66b2ba10191151060d90bc481c6
Connie Leung <cleung334@gmail.com>
2023-07-22 23:34:43 +0800

    chore: delete unused type

--- projects/pokemon-demo-13/src/app/pokemon/types/pokemon-tab.type.ts
+++
@@ -1,4 +0,0 @@
-import { PokemonAbilitiesComponent } from '../pokemon-abilities/pokemon-abilities.component';
-import { PokemonStatsComponent } from '../pokemon-stats/pokemon-stats.component';
-
-export type DynamicComponents = (typeof PokemonAbilitiesComponent | typeof PokemonStatsComponent)[];
